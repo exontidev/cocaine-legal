8c5b64058793c7f5974fa8faa0f50c7e1f800a6d
exontidev <me@exonti.dev>
2026-07-15 00:39:47 +0200

    frontend: without skew

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -32,9 +32,12 @@ function App() {
 
       <main className="sm:ml-[38px]">
         <section className="pt-24 sm:pt-[60px] border-b-2 dotted-bg flex justify-center h-[950px]">
-          <div className="flex bg-white h-fit mt-14 sm:mt-40 justify-center border-4 p-5 m-10 flex-col gap-2 skew-y-12 w-[1000px] shadow-[8px_8px_0px_rgba(0,0,0,1)]">
+          <div className="flex bg-white h-fit mt-14 sm:mt-40 justify-center border-4 p-5 m-10 flex-col gap-2 w-[1000px] shadow-[8px_8px_0px_rgba(0,0,0,1)]">
             <h3 className="font-anton text-4xl sm:text-6xl lg:text-7xl xl:text-8xl break-all ">UUUUUUOOOHHHHHHHOOHOHHOOHHOOHOHHOHOHHOOHHOOHOHHOHOHOHOHHOOHOOHOHOHOHOHOHOOHOHHOOHHOHOHOHHOOHOHOHOOOO</h3>
             <p className="font-oxygen-mono">cringe edgy emails. ownership settled on solana. permissionless. magnificent.</p>
+            <section className="-skew-x-12">
+              <button className="border-2 font-oxygen-mono p-2">button to suck cock</button>
+            </section>
           </div>
         </section>
       </main>
